73261d38212654ba7d2546cade1f3136a50a196f
Nic Robbins <monicamfair@gmail.com>
2022-03-10 20:51:30 -0600

    week 4 b4 data viz

---
+++ week4/dataviz/pyplot.py
@@ -0,0 +1,22 @@
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+# %%
+x1 = [0.1, 0.2, 0.3, 0.4]
+y1 = [0.5, 2.2, 3.5, 8.2]
+x2 = [0.1, 0.5, 0.6, 0.7]
+y2 = [1, 4, 9, 16]
+# %%
+plt.plot(x1, y1, label='first plot')
+plt.plot(x2, y2, label='second plot')
+plt.legend()
+# %%
+num = 100
+x = np.linspace(0, 20, num)
+print(x)
+# %%
+y = np.random.rand(num)
+print(y)
+# %%
+plt.plot(x, y)
+# %%
